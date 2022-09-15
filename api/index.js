7230794e38611d1db4520c471fc65c3248887fd6
Orungbemi Favour <favourorungbemi310@gmail.com>
2022-09-15 13:38:34 +0100

    update vercel

--- api/index.js
+++ api/index.js
@@ -3,8 +3,7 @@ const path = require("path");
 const fs = require('fs/promises');
 const app = express();
 const generateUrl = require('../Middlewares/generateUrl');
-
-app.use(express.static(path.resolve('../Public')))
+app.use(express.static(path.join('__dirname, ../Public')))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.get('/',async(req,res)=>{
